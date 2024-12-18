ba05f82ddac45323c395434518f847108e546308
Indra Fajar Nurwahid <fajarnurwahid99@gmail.com>
2024-12-18 07:21:55 +0700

    fix: Enable cookie support for axios
    Enable cookie support in the axios instance to allow
    cross-site requests with authentication.  This allows
    the application to maintain session state across
    multiple requests.

--- src/commons/utils/axios.util.ts
+++ src/commons/utils/axios.util.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create();
+axiosInstance.defaults.withCredentials = true;
 axiosInstance.interceptors.request.use(
   (config) => {
     config.headers.microserviceToken = process.env.MICROSERVICE_TOKEN;
